7b13242c96883ead697cc7a5b5691c3a5cfce4f8
Perry Raskin <perryraskin@gmail.com>
2019-07-23 23:07:40 -0400

    Get Mishna email template

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const handlebars = require('handlebars');
+const fs = require('fs')
 
 function renderTemplateFromData(source, data) {
   var template = handlebars.compile(source);
@@ -7,4 +8,26 @@ function renderTemplateFromData(source, data) {
   var html    = template(data);
   return html;
 }
-console.log(renderTemplateFromData());
+//console.log(renderTemplateFromData());
+
+function getMishnaEmailTemplateFromFile() {
+  const file = './templates/ms.html';
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) return reject(err);
+      return resolve(data);
+    });
+  });
+}
+getMishnaEmailTemplateFromFile()
+.then(data => console.log(data))
+.catch(err => console.error(err));
+
+/*
+function runMS() {
+  const msTemplate = getMishnaEmailTemplateFromFile();
+  const msData = getMishnaData();
+  const rendered = renderTemplateFromData(msTemplate, msData);
+}
+*/
