ee9ebfc9b35426b3530fc4d8b1d9b7dc4debe489
Joey Pinhas <jpinhas97@gmail.com>
2019-07-25 21:52:57 -0400

    Add structured instructions

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const handlebars = require('handlebars');
 const fs = require('fs')
 
+/*
+  renderTemplateFromData is designed to take in an email template, some data
+  to render it with, and simply returns the rendered html.
+*/
 function renderTemplateFromData(source, data) {
   var template = handlebars.compile(source);
-
-  var context = {title: "My New Post", body: "This is my first post!"};
-  var html    = template(data);
+  var html = template(data);
   return html;
 }
-//console.log(renderTemplateFromData());
 
-function getMishnaEmailTemplateFromFile() {
-  const file = './templates/ms.html';
-
+/*
+  getDataFromFileAsync takes as its parameter a file path and returns a promise,
+  which, when it returns, returns the data of the file. It can be used to open
+  and read an email template file.
+*/
+function getDataFromFileAsync(file) {
   return new Promise((resolve, reject) => {
     fs.readFile(file, 'utf8', function(err, data) {
       if (err) return reject(err);
@@ -20,14 +24,31 @@ function getMishnaEmailTemplateFromFile() {
     });
   });
 }
-getMishnaEmailTemplateFromFile()
-.then(data => console.log(data))
-.catch(err => console.error(err));
+
+function getDataByProgramByDateAsync(program, date) {
+  // TODO: hit the api with a request to get the data for that date
+  // api route is: https://api.tanachstudy.com/${program}/schedule/${date}
+  // return fetch...
+}
+
+function transformAPIDataToTemplateData(apiData) {
+  // TODO: pull out values from the response object to pass to the template
+}
+
 
 /*
-function runMS() {
-  const msTemplate = getMishnaEmailTemplateFromFile();
-  const msData = getMishnaData();
-  const rendered = renderTemplateFromData(msTemplate, msData);
-}
+  run takes as its parameter a context object, which has data about which
+  program we wish to render. It should be called by main, which should set this
+  context object.
 */
+function run(context) {
+  // TODO: get the program name and path to template from context
+  // TODO: get the program's template from its file
+  // TODO: get the date on which we wish to run this on from context
+  // TODO: get the data for the program at the given date from the TS api
+  // TODO: render the template and return it
+}
+
+function main() {
+
+}
